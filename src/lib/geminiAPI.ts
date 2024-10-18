0883a19f497e9a475212434c286ae571e6756b66
CoWeDeveloper <owaisilyas22@gmail.com>
2024-10-18 16:55:15 +0500

    Minor changes on API

--- src/lib/geminiAPI.ts
+++ src/lib/geminiAPI.ts
@@ -1,6 +1,6 @@
 export async function fetchDataAnalysis(userInput: string) {
     const response = await fetch(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${process.env.GEMINIAI_API_KEY}`,
+        process.env.API_URL || '',
         {
             method: 'POST',
             headers: {
